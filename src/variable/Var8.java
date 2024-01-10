3cf433f66e74fae908f628f70013d7fbda0bad7c
정현욱 <jeonghyeonuk@jeonghyeon-ugs-MacBook-Air.local>
2024-01-10 23:42:57 +0900

    [인프런] 김영한의 자바 입문 섹션 0 ~ 2까지 학습

---
+++ src/variable/Var8.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; // -128 ~ 127; 2의 8승
+        short s = 32767; // ~32,768 ~ 32,767 2의 16승
+        int i = 2147483647; // -2,147,483,648 ~ 2,147,483,647 (약 20억) 2의 32승
+        //  -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807 2의 64승
+        long l = 9223372036854775807L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+
+        //실무에서 많이쓰는 타입
+        // int, long, double, boolean, String
+    }
+}
